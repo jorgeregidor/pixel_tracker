68a21787760f143c7c84892eb650083d13c7cf3c
Jorge Regidor <jorgeregidor@gmail.com>
2018-12-06 20:24:48 +0000

    intalled rspec

---
+++ spec/controllers/providers_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe ProvidersController do
+	
+	describe "ProductsController" do
+
+  	describe "logged" do
+
+			
+  		
+  		describe "GET index" do
+
+
+		    it "correct response" do
+		      expect(true).to eq(true)
+		    end
+
+		  end
+
+		 
+		end
+	end
+end
