bc28445fa77ad592f98ff2f14ef1e1df6443df5b
Jorge Regidor <jorgeregidor@gmail.com>
2018-12-06 20:57:51 +0000

    added dependences of models

--- app/models/pixel.rb
+++ app/models/pixel.rb
@@ -1,6 +1,7 @@
 class Pixel < ApplicationRecord
   belongs_to :provider
-  has_many :conversions
+
+  has_many :conversions, dependent: :destroy
 
   validates_presence_of :name
   validates :name, uniqueness: {scope: :provider_id}
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,6 @@
 class Provider < ApplicationRecord
   belongs_to :user
-  has_many :pixels
+  has_many :pixels, dependent: :destroy
 
   validates_presence_of :name
   validates :name, uniqueness: {scope: :user_id}
