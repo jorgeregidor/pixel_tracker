d1a910ebfa443985e3a1fcdee7a3f8185168b999
Jorge Regidor <jorgeregidor@gmail.com>
2018-12-06 19:29:40 +0000

    created pixels controller

---
+++ app/controllers/pixels_controller.rb
@@ -0,0 +1,12 @@
+class PixelsController < ApplicationController
+  
+	# POST pixels/new
+	# Create a new pixel for a provider
+  def new
+  end
+
+  # GET images/conversion
+  # Create a new conversion por a pixel
+  def conversion
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post 'pixels/new', to: 'pixels#new'
+	get 'images/conversion', to: 'pixels#converion'
+
   resources :providers, except: [:index]
 	
   devise_for :users
